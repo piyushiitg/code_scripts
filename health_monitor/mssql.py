bfbfa279789929494f61119cd960537c620282cc
piyush singhai <piyush.singhai@scalearc.com>
2015-04-22 14:31:25 +0530

    remove old files

--- health_monitor/mssql.py
+++
@@ -1,8 +0,0 @@
-from database import Database
-class Mssql(Database):
-    def __init__(self, kwargs):
-        super(Mssql, self).__init__(kwargs)
-
-    def query_monitor(self, inputdata):
-        return "MSSQL query Monitor"
-         
